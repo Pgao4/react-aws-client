91840b8b62e974b9ec57af9019f961bd19925a57
pgao4 <pgao4@ualberta.ca>
2022-10-29 23:48:23 -0600

    add admin/category/create page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,13 +1,32 @@
 import dynamic from 'next/dynamic'
 import withAdmin from '../withAdmin'
+import Link from 'next/link'
 
 const Layout = dynamic(
   () => import('../../components/Layout'),
   { ssr: false }
 )
 
-const Admin = ({user, token}) => {
-    return <Layout>{JSON.stringify(user)}</Layout>
-  }
-  
+const Admin = ({ user }) => {
+  return (
+    <Layout>
+      <h1>Admin Dashboard</h1>
+      <br />
+      <div className="row">
+        <div className="col-md-4">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <Link href="/admin/category/create">
+                <a className='nav-link'>Create Category</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-8">
+
+        </div>
+      </div>
+    </Layout>)
+}
+
 export default withAdmin(Admin)
