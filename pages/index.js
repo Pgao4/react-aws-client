69571f62fae54986be3904a63bb1d97aec8c761f
pgao4 <pgao4@ualberta.ca>
2022-10-30 22:38:06 -0600

    show categories on index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,53 @@ const Layout = dynamic(
   () => import('../components/Layout'),
   { ssr: false }
 )
+import axios from 'axios'
+import { API } from '../config'
+import Link from 'next/link'
 
-const Home = () => {
-    return <Layout>Welcome to Next.js!</Layout>
+const Home = ({ categories }) => {
+  const listCategories = () => categories.map((c, i) => (
+    <Link href="/">
+      <a style={{ border: '1px solid red' }} className="bg-light p-3 col-md-4">
+        <div>
+          <div className="row">
+            <div className="col-md-4">
+              <img className="pr-3"
+                src={c.image && c.image.url}
+                alt={c.name}
+                style={{ width: '100px', height: 'auto' }} />
+            </div>
+            <div className="col-md-8">
+              <h3>{c.name}</h3>
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  ))
+
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="font-weight-bold">Browse Tutorials/Courses</h1>
+          <br />
+        </div>
+      </div>
+
+      <div className="row">
+        {listCategories()}
+      </div>
+
+
+    </Layout>
+  )
+}
+
+Home.getInitialProps = async () => {
+  const response = await axios.get(`${API}/categories`)
+  return {
+    categories: response.data
   }
-  
+}
 export default Home
